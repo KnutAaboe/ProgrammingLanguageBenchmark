1c0c8e66c1b05548adf526612086455848798fce
Kent Inge F. Simonsen <kent.simonsen@tietoevry.com>
2023-04-27 11:15:40 +0200

    really run rust

--- Rust/binary-tree-azure-function/HttpTriggerBT/index.js
+++ Rust/binary-tree-azure-function/HttpTriggerBT/index.js
@@ -1,13 +1,18 @@
+let binarytree = require("binary-tree-rust");
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    console.log(binarytree);
+    console.log(binarytree.__wasm);
+    console.log("---------- generate tree ----------")
+    try{
+        let treeRes = binarytree.generateTree();
+        console.log("tree:", treeRes);
+        context.res = {
+            status: 200, /* Defaults to 200 */
+            body: treeRes
+        };
+    } catch (error) {
+        console.error(error);
+    }
 }
