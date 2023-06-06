b6a455dff25f9bf856344516b266c1b38b6ea4af
Sassan Zomorodi <sassanzomorodi@yahoo.com>
2023-06-06 13:51:17 +0200

    Changed structure of Rust folder
    Upstream merge

--- Rust/simple-azure-function/HttpTriggerS/index.js
+++
@@ -1,9 +0,0 @@
-let rusty = require("simple-rust");
-
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res = {
-        body: rusty.greet()
-    };
-};
